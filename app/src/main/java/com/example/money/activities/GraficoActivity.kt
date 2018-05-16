91a96fbaab3a087accf7ccd3d66ed7da1e1a690f
emontesinos <emontesinos812@gmail.com>
2018-05-16 20:07:02 +0200

    personalizabdo Grafico

--- app/src/main/java/com/example/money/activities/GraficoActivity.kt
+++ app/src/main/java/com/example/money/activities/GraficoActivity.kt
@@ -9,19 +9,19 @@ import android.os.Bundle
 import com.example.money.R
 import com.example.money.database.DBManagerMoney
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.listener.OnChartGestureListener
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 
 
 class GraficoActivity : AppCompatActivity() {
 
-//    var plot: PlotPlanitoXY? = null
-//    var pantalla: LinearLayout? = null
-//    var context: Context? = null
+
     var db: DBManagerMoney? = null
     lateinit var chart:LineChart
 
@@ -30,37 +30,31 @@ class GraficoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_grafico)
         chart=findViewById<LineChart>(R.id.chart)
 
-        //chart.onChartGestureListener
-        //chart.setOnChartValueSelectedListener(this)
+
 
         chart.isDragEnabled
         chart.setScaleEnabled(false)
-
+////////////////////////////////////////////////////////////
         val ingresos:ArrayList<Entry> = ArrayList<Entry>()
-
-        ingresos.add(Entry(1F,60f))
-        ingresos.add(Entry(2F,50f))
-        ingresos.add(Entry(3F,70f))
-        ingresos.add(Entry(4F,30f))
-        ingresos.add(Entry(5F,60f))
-        ingresos.add(Entry(6F,50f))
-        ingresos.add(Entry(7F,60f))
+        ingresos.add(Entry(0F,60f))
+        ingresos.add(Entry(1F,50f))
+        ingresos.add(Entry(2F,70f))
+        ingresos.add(Entry(3F,30f))
+        ingresos.add(Entry(4F,60f))
         val linea1=LineDataSet(ingresos,"Ingresos")
         linea1.color= Color.GREEN
         linea1.lineWidth= 3F
         linea1.valueTextSize=12f
         linea1.fillAlpha=110
 
-
+/////////////////////////////////////////////////////////
         val egresos:ArrayList<Entry> = ArrayList<Entry>()
+        egresos.add(Entry(0F,50f))
+        egresos.add(Entry(1F,40f))
+        egresos.add(Entry(2F,60f))
+        egresos.add(Entry(3F,20f))
+        egresos.add(Entry(4F,50f))
 
-        egresos.add(Entry(1F,50f))
-        egresos.add(Entry(2F,40f))
-        egresos.add(Entry(3F,60f))
-        egresos.add(Entry(4F,20f))
-        egresos.add(Entry(5F,50f))
-        egresos.add(Entry(6F,40f))
-        egresos.add(Entry(7F,50f))
         val linea2=LineDataSet(egresos,"Gastos")
         linea2.color= Color.RED
         linea2.valueTextSize=12f
@@ -68,43 +62,36 @@ class GraficoActivity : AppCompatActivity() {
         linea2.fillAlpha=110
 
 
-
-
-
-
-
         val dataSet=java.util.ArrayList<ILineDataSet>()
         dataSet.add(linea1)
         dataSet.add(linea2)
-
-
         val datas:LineData=LineData(dataSet)
-
         chart.data=datas
 
 
 
+//AGREGAMOS
+        val values:ArrayList<String> = ArrayList()
+        values.add("Enero")
+        values.add("Febrero")
+        values.add("Marzo")
+        values.add("Abril")
+        values.add("Mayo")
+        val xAxis=chart.xAxis
+        xAxis.valueFormatter = MiAxis(values)
 
-
-
-
-       /* pantalla = findViewById<LinearLayout>(R.id.pantalla)
-        context = this
-        db = DBManagerMoney(this)
-        val x = floatArrayOf(2f, 3f, 4f, 5f)
-        val y = floatArrayOf(4f, 5f, 6f, 9f)
-        val a = floatArrayOf(2f, 3f, 4f, 5f)
-        val b = floatArrayOf(3f, 4f, 5f, 8f)
-        plot = PlotPlanitoXY(context, "Grafico de Ingresos y Gastos", "Ingresos", "gastos")
-        plot!!.SetSerie1(x, y, "Ingresos", 5, true)//x and y simpre en ese orden,grap1=nombre de la leyenda,5=tamaño del punto,true=si une losmpuntos con un alinea
-        plot!!.SetHD(true)//suavise ls border
-        plot!!.SetTouch(true)//funcione con el touch
-        plot!!.SetSizeLeyend(20)
-        plot!!.SetSizeTitulo(40)
-        plot!!.SetSerie2(a, b, "Egresos", 5, true, 1)
-        pantalla!!.addView(plot)*/
     }
 
+
+
+    class MiAxis(values: ArrayList<String>) : IAxisValueFormatter{
+        var mvalues:ArrayList<String> = values
+
+        override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+            return mvalues[value.toInt()]
+        }
+
+    }
 
     @SuppressLint("Recycle")
     fun sumaIngresos(): String {
@@ -125,4 +112,7 @@ class GraficoActivity : AppCompatActivity() {
         }
         return total.toString()
     }
+
+
 }
+
